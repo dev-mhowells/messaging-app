88654ecb4f77b50106d6d5a714bd1d763843131a
Mike <dev.mhowells@gmail.com>
2023-02-14 17:00:03 +0000

    removed unused imports from ProtectedRoutes.js

--- src/ProtectedRoutes.js
+++ src/ProtectedRoutes.js
@@ -1,7 +1,5 @@
-import { Outlet, Route, Navigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import React from "react";
-import Login from "./DemoLogin";
-import Messenger from "./Messenger";
 
 // need to specify outlet here, not the name of the component (Messenger) otherwise
 // props don't pass.
